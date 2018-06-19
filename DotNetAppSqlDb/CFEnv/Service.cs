2c58d5c530f30620047b48228ad58c4a42b1feed
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-06-18 21:14:59 -0400

    Initial commit after adding dynamic connection string

---
+++ DotNetAppSqlDb/CFEnv/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetAppSqlDb.CFEnv
+{
+    public class Service
+    {
+        public string ServiceName { get; set; }
+        public string BindingName { get; set; }
+        public string Label { get; set; }
+        public List<string> Tags { get; set; }
+        public string Plan { get; set; }
+        public Dictionary<string, string> Credentials { get; set; }
+    }
+}
